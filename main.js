1dd57c0423b96e95faf35466d6a6b2c33c964774
fastboy <giagiang200@gmail.com>
2022-01-24 11:26:31 +0700

    dequy

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
-// xóa các phần tử  trùng trong mảng
+// // xóa các phần tử  trùng trong mảng
 
-var array = ['a','b','c','a','b','c']
-console.log([...(new Set(array))]);
+// var array = ['a','b','c','a','b','c']
+// console.log([...(new Set(array))]);
+
+fuction Dequy(num){
+    num--;
+
+    if (num<0 ){
+        return;
+    }
+    Dequy
+}
+
+Dequy(10);
