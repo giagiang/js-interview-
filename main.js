fb7e047c95b3e469f6ea5cf6aab0ea67976978ed
fastboy <giagiang200@gmail.com>
2022-01-24 14:35:06 +0700

    dequy

--- main.js
+++ main.js
@@ -3,13 +3,33 @@
 // var array = ['a','b','c','a','b','c']
 // console.log([...(new Set(array))]);
 
-fuction Dequy(num){
-    num--;
 
-    if (num<0 ){
-        return;
-    }
-    Dequy
-}
+// xác định điểm dừng
+//  logic handle => tạo ra điểm dừng 
+// function  Recursion(){
+//     num-- ;
+//     if(num < 0){
+//         //dừng 
+//         //...
+//     }
+//     Recursion();
 
-Dequy(10);
+// }
+// Recursion(10);
+
+
+
+
+// function countDown(num){
+//     if(num>0){
+//         console.log(num);
+//         return countDown(num - 1);
+
+//     }
+//     return num;
+
+// }
+// countDown(3);
+
+
+
