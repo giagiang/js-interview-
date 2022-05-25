17e9b903efe2799efaaf174d7ce34a664a839322
fastboy <giagiang200@gmail.com>
2022-05-25 21:18:27 +0700

    toán tử kiểu dữ  liệu

--- main.js
+++ main.js
@@ -49,3 +49,120 @@
 
 // toán tử so sánh 
 
+
+// var  a= 1 ;
+// var b = 2 ;
+
+// if (a < b ){
+//     console.log('điều kiện này chạy sai rồi ');
+// }else{
+//     console.log('điều kiện này chạy đúng rồi ')
+// }
+
+
+
+// kiểu dữ liệu Boolean
+
+// var a = 1 ;
+// var b = 2 ;
+
+// var isSuccess = a >  b ; 
+// console.log( isSuccess);
+
+
+
+
+// // câu lệnh if else 
+// var isSuccess = 2 > 1 ;
+// if (isSuccess){
+//     console.log ('điều khiển đúng ');
+// }else{
+//     console.log('điều kiển sai ');
+// }
+
+// //toán tử logical 
+// 1 &&  - and 
+// 2. || - or  
+// 3. !  - not 
+
+
+// var a = 1 ;
+// var b = 2 ;
+// var c = 3 ;
+
+// if(!(a < 0 )){
+//     console.log('điều kiện đúng !');
+// }
+
+
+// kiểu dữ liệu trong javascript
+
+// 1. dữ liệu nguyên thủy - primitive Data 
+// - number 
+// -string 
+// - boolean 
+// -undefined 
+// -null 
+// -symbol
+
+// 2. dữ liệu phức tạp  - complex data  
+// - function 
+// - object 
+
+  
+// var myFunction = function()  {
+//      alert('hi, chào cậu ');
+// }
+// // myFunction();
+
+// var myObject = { 
+//     name : 'gia giang',
+//     age : 19 ,
+//     address: 'hà nội ',
+//     myFunction: function(){
+//     }
+// }; 
+// console.log('myObject', myObject);
+
+// var myAray = [
+//     'javascript',
+//     'ruby',
+//     'php',
+// ];
+
+
+// toán tử so sánh  
+
+// ===
+// !==
+// /
+
+// var a  =  1;
+// var b  = '1' ;
+
+// console.log(a === b);
+// khi sử dụng === thì nõ sẽ so sánh cả value và datatype
+
+
+// var  a = 1 ;
+// var  b = '1';
+// console.log(a !== b );
+
+
+
+// hàm (fuction) trong javascript
+// 1hàm
+// - hàm là một khối mã 
+// - làm một việc cụ thể 
+// 2 loại hàm 
+// -  Built-in 
+// - tự định nghĩa 
+// 3 tính chất 
+// - không thực thi khi định nghĩa 
+// - sẽ thực thi khi được gọi 
+// - có thể nhận tham số 
+// - có thể trả về một giá trị 
+// 4 tạo hàm đầu tiên 
+
+
+
